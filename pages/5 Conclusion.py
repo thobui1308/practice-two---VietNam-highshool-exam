4bf302ce1f5f4512ccad05b7869fcdb527f96634
Thobui <thobui1308@gmail.com>
2024-03-29 14:52:47 +0700

    feat: do practice

--- pages/5 Conclusion.py
+++ pages/5 Conclusion.py
@@ -2,19 +2,8 @@ import streamlit as st
 import requests 
 from streamlit_lottie import st_lottie
 
-st.title(':kite: Conclusion')
+st.title(':school: Conclusion')
 
-def load_lottieurl(url: str):
- r = requests.get(url)
- if r.status_code != 200:
-    return None
- return r.json()
-lottie_school = load_lottieurl(
- "https://lottie.host/8937418e-9e58-4fb7-85c7-2857144f0776/dcZjmV9IJZ.json"
-)
-st_lottie(lottie_school, height=500)
-
-st.markdown('---')
 st.write(
  """
  
@@ -27,3 +16,13 @@ In conclusion, the analysis of the national high school exam scores from 2018 to
 * Geographical Disparities: There may be noticeable differences in exam scores across different provinces or regions. Understanding these geographical disparities can help in addressing educational inequalities and providing targeted support to underperforming areas.
  """
 )
+
+def load_lottieurl(url: str):
+ r = requests.get(url)
+ if r.status_code != 200:
+    return None
+ return r.json()
+lottie_school = load_lottieurl(
+ "https://lottie.host/8937418e-9e58-4fb7-85c7-2857144f0776/dcZjmV9IJZ.json"
+)
+st_lottie(lottie_school, height=500)
